b86cd384c75eaa8eb415478b9ff553d0b5767dd3
realvisper <xelorahub@gmail.com>
2025-09-13 22:00:08 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,3 @@
+if game.PlaceId == 86076978383613 or game.PlaceId == 103754275310547 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/realvisper/script/refs/heads/main/huntyz.lua"))()
+end
